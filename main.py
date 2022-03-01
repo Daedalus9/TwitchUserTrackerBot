cb3182e74a8a407d37c9709ad474eb70c3e957ae
Daedalus9 <50534107+Daedalus9@users.noreply.github.com>
2022-03-01 13:25:58 +0100

    fix: bugfix on search

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from email import message
 import telegram
 from telegram.ext import CommandHandler
 from telegram.ext import Updater
@@ -27,33 +28,37 @@ def deleteId(update):
 
 def search(update, context):
     chat_id=update.effective_chat.id
+    message=update.message.text
     us = open("list.txt", "r")
     if chat_id not in ids:
         context.bot.send_message(chat_id, "Attendi, potrebbero volerci fino a 5 minuti (per interrompere la ricerca corrente utilizza il comando /stop)")
-        try:
+        if(message!="/track"):
             addId(update)
             user=update.message.text.split(" ",1)[1]
             counter=0
             for line in us:
-                if chat_id in ids:
-                    url = "https://tmi.twitch.tv/group/user/"+line.lower()[:-1]+"/chatters"
-                    r=requests.get(url).json()
-                    chat=r.get('chatters')['viewers']
-                    if(user in chat):
-                        context.bot.send_message(chat_id, user + " sta guardando " + line[:-1])
-                        counter+=1
-                        break
-                else:
-                    context.bot.send_message(chat_id, "Ricerca interrotta")
-                    return
+                try:
+                    if chat_id in ids:
+                        url = "https://tmi.twitch.tv/group/user/"+line.lower()[:-1]+"/chatters"
+                        r=requests.get(url).json()
+                        chat=r.get('chatters')['viewers']
+                        if(user in chat):
+                            context.bot.send_message(chat_id, user + " sta guardando " + line[:-1])
+                            counter+=1
+                            break
+                    else:
+                        context.bot.send_message(chat_id, "Ricerca interrotta")
+                        return
+                except:
+                    continue
             if(counter<1):
                 context.bot.send_message(chat_id, "L'utente " + user + " non sta guardando nessun canale Twitch, al momento")
             context.bot.send_message(chat_id, "Fine analisi")
             us.close()
             deleteId(update)
-        except:
+        else:
             context.bot.send_message(chat_id, "Nessun nickname specificato. Utilizza il comando come segue: /track nickname")
-            deleteId(update)
+            #deleteId(update)
     else:
         context.bot.send_message(chat_id, "Stai già facendo una ricerca. Attendi che finisca, prima di chiederne un'altra oppure utilizza il comando /stop per interrompere la ricerca corrente")
     
