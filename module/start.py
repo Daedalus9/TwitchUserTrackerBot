47097b6a023ada5a531816175ca32c340fde7c90
Daedalus9 <longo.luca94@hotmail.it>
2022-03-01 19:14:27 +0100

    feat: welcome message changed

--- module/start.py
+++ module/start.py
@@ -1,3 +1,3 @@
 def start(update, context):
     chat_id=update.effective_chat.id
-    context.bot.send_message(chat_id, "Benvenuto")
+    context.bot.send_message(chat_id, "Benvenuto! Utilizza il comando /track <nickname> per iniziare la ricerca")
