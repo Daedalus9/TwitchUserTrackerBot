74d2f509422903213eccaaa8859d917f1a862ef6
Daedalus9 <50534107+Daedalus9@users.noreply.github.com>
2022-03-07 11:34:06 +0100

    refactor: updated text file with search.py text

--- var/text.py
+++ var/text.py
@@ -4,3 +4,7 @@ ERROR_NICKNAME = "Nessun nickname specificato. Utilizza il comando come segue: /
 WARNING_MESSAGE = "Stai già facendo una ricerca. Attendi che finisca, prima di chiederne un'altra oppure utilizza il comando /stop per interrompere la ricerca corrente"
 WELCOME_MESSAGE = "Benvenuto! Utilizza il comando /track <nickname> per iniziare la ricerca"
 WARNING_NO_SEARCH_MESSAGE  = "Non stai facendo nessuna ricerca, al momento"
+WATCH_TEXT = " sta guardando "
+USER_TEXT = "L'utente "
+NOT_FIND_MESSAGE = " non sta guardando nessun canale Twitch, al momento"
+STOP_ANALYSIS_MESSAGE = "Fine analisi"
